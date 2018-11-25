e1dba1afb0a658760a2711e86c157e13286bfc34
KarolisKaj <karolis.kajenas1@gmail.com>
2018-11-25 13:45:28 +0200

    imputed data

--- src/ml/learn/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/categorical_data.py
+++ src/ml/learn/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/categorical_data.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv("C:\\Users\\WarHorse\\SourceControl\\MLSnake\\src\\ml\\learn\Machine Learning A-Z Template Folder\\Part 1 - Data Preprocessing\\Data.csv")
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, 0:3].values
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = Imputer(missing_values = np.nan, strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
