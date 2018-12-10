ba5e4a75811e22b82943bf3888e4da85362cd94d
KarolisKaj <karolis.kajenas1@gmail.com>
2018-12-10 21:56:25 +0200

    need to normalize data

--- src/ml/learner.py
+++ src/ml/learner.py
@@ -6,6 +6,14 @@ class learner:
         self.df = pd.read_json(trainingSetPath)
 
     def getModel(self):
+        enumIndexes = [3, 4, 5, 6, 11]
+        # Normalize
+        for value in self.df:
+            if(not value in enumIndexes):
+                print([self.df[value]])
+                self.df[value] = tf.keras.utils.normalize([self.df[value]])
+
+        # Transform
         neighbour = {'Food': 4, 'Empty': 3,'Wall': 2, 'Body':1 }
         self.df[3] = [neighbour[item] for item in self.df[3]] 
         self.df[4] = [neighbour[item] for item in self.df[4]] 
@@ -13,6 +21,7 @@ class learner:
         self.df[6] = [neighbour[item] for item in self.df[6]]
 
         move = {'Right': 1, 'Left': 2,'Top': 3, 'Bottom':4 }
-        self.df[11] = [move[item] for item in self.df[11]] 
+        self.df[11] = [move[item] for item in self.df[11]]
+
         print(self.df)
 
