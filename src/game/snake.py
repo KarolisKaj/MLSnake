cdb9d533ac7aa359d1bba22d6a5848e2a5f23d74
KarolisKaj <karolis.kajenas1@gmail.com>
2018-12-16 14:40:51 +0200

    automated gameplay to learn on it's own.

--- src/game/snake.py
+++ src/game/snake.py
@@ -1,107 +1,108 @@
-from turtle import *
+import turtle
 from random import randrange
 from freegames import square, vector
 
-food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
+class snakeGame:
+    def __init__(self):
+        self.food = vector(0, 0)
+        self.snake = [vector(10, 0)]
+        self.aim = vector(0, -10)
 
-def setFoodCoordinates():
-    food.x = randrange(-19, 19) * 10
-    food.y = randrange(-19, 19) * 10
-    if(food in snake):
-        setFoodCoordinates()
+    def setFoodCoordinates(self):
+        self.food.x = randrange(-19, 19) * 10
+        self.food.y = randrange(-19, 19) * 10
+        if(self.food in self.snake):
+            self.setFoodCoordinates()
 
-def change(x, y):
-    aim.x = x
-    aim.y = y
+    def change(self, x, y):
+        self.aim.x = x
+        self.aim.y = y
 
-def inside(coordinates):
-    return -200 < coordinates.x < 200 and -200 < coordinates.y < 200
+    def inside(self, coordinates):
+        return -200 < coordinates.x < 200 and -200 < coordinates.y < 200
 
-def move(track, predict):
-    head = snake[-1].copy()
-    
-    # Learn only by human
-    # track(len(snake), len(snake) * 10, whereIsCoordinate(vector(head.x - 10, head.y)), whereIsCoordinate(vector(head.x + 10, head.y)), whereIsCoordinate(vector(head.x, head.y + 10)), whereIsCoordinate(vector(head.x, head.y - 10)), (head.x, head.y), (food.x, food.y), direction)
-    # Play
-    stepData = track(len(snake), len(snake) * 10, whereIsCoordinate(vector(head.x - 10, head.y)), whereIsCoordinate(vector(head.x + 10, head.y)), whereIsCoordinate(vector(head.x, head.y + 10)), whereIsCoordinate(vector(head.x, head.y - 10)), (head.x, head.y), (food.x, food.y), None)
-    performPredictedMove(predict(stepData))
-    direction = moveDirection(head, aim)
-    track(len(snake), len(snake) * 10, whereIsCoordinate(vector(head.x - 10, head.y)), whereIsCoordinate(vector(head.x + 10, head.y)), whereIsCoordinate(vector(head.x, head.y + 10)), whereIsCoordinate(vector(head.x, head.y - 10)), (head.x, head.y), (food.x, food.y), direction)
-    head.move(aim)
+    def move(self, track, predict):
+        head = self.snake[-1].copy()
+        # Play
+        stepData = track(len(self.snake), len(self.snake) * 10, self.whereIsCoordinate(vector(head.x - 10, head.y)), self.whereIsCoordinate(vector(head.x + 10, head.y)), self.whereIsCoordinate(vector(head.x, head.y + 10)), self.whereIsCoordinate(vector(head.x, head.y - 10)), (head.x, head.y), (self.food.x, self.food.y), None)
+        self.performPredictedMove(predict(stepData))
+        # Learn
+        direction = self.moveDirection(head, self.aim)
+        track(len(self.snake), len(self.snake) * 10, self.whereIsCoordinate(vector(head.x - 10, head.y)), self.whereIsCoordinate(vector(head.x + 10, head.y)), self.whereIsCoordinate(vector(head.x, head.y + 10)), self.whereIsCoordinate(vector(head.x, head.y - 10)), (head.x, head.y), (self.food.x, self.food.y), direction)
+        head.move(self.aim)
 
-    if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
-        update()
-        return
+        if not self.inside(head) or head in self.snake:
+            square(head.x, head.y, 9, 'red')
+            turtle.update()
+            return
 
-    snake.append(head)
+        self.snake.append(head)
 
-    if head == food:
-        print('Snake size :', len(snake))
-        setFoodCoordinates()
-    else:
-        snake.pop(0)
+        if head == self.food:
+            print('Snake size :', len(self.snake))
+            self.setFoodCoordinates()
+        else:
+            self.snake.pop(0)
 
-    clear()
+        turtle.clear()
 
-    for body in snake:
-        square(body.x, body.y, 9, 'black')
+        for body in self.snake:
+            square(body.x, body.y, 9, 'black')
 
-    square(food.x, food.y, 9, 'green')
-    update()
-    ontimer(lambda: move(track, predict), 100)
+        square(self.food.x, self.food.y, 9, 'green')
+        turtle.update()
+        turtle.ontimer(lambda: self.move(track, predict), 100)
 
 
-def start(track, predict):
-    setup(420, 420, 370, 0)
-    hideturtle()
-    tracer(False)
-    listen()
-    onkey(lambda: change(10, 0), 'Right')
-    onkey(lambda: change(-10, 0), 'Left')
-    onkey(lambda: change(0, 10), 'Up')
-    onkey(lambda: change(0, -10), 'Down')
-    move(track, predict)
-    done()
+    def start(self, track, predict):
+        turtle.setup(420, 420, 370, 0)
+        turtle.hideturtle()
+        turtle.tracer(False)
+        turtle.listen()
+        # turtle.onkey(lambda: self.change(10, 0), 'Right')
+        # turtle.onkey(lambda: self.change(-10, 0), 'Left')
+        # turtle.onkey(lambda: self.change(0, 10), 'Up')
+        # turtle.onkey(lambda: self.change(0, -10), 'Down')
+        self.move(track, predict)
+        turtle.done()
 
-#{'Right': 0, 'Left': 1,'Top': 2, 'Bottom': 3 }
-def performPredictedMove(predictedDirection):
-    if(predictedDirection == 0):
-        right()
-    elif(predictedDirection == 1):
-        left()
-    elif(predictedDirection == 2):
-        top()
-    elif(predictedDirection == 3):
-        bottom()
 
-def right():
-    change(10, 0)
-def left():
-    change(-10, 0)
-def top():
-    change(0, 10)
-def bottom():
-    change(0, -10)
+    #{'Right': 0, 'Left': 1,'Top': 2, 'Bottom': 3 }
+    def performPredictedMove(self, predictedDirection):
+        if(predictedDirection == 0):
+            self.right()
+        elif(predictedDirection == 1):
+            self.left()
+        elif(predictedDirection == 2):
+            self.top()
+        elif(predictedDirection == 3):
+            self.bottom()
 
-def moveDirection(head, aim):
-    if(aim.x > 0):
-        return "Right"
-    elif(aim.x < 0):
-        return "Left"
-    elif(aim.y < 0):
-        return "Bottom"
-    else:
-        return "Top"
+    def right(self):
+        self.change(10, 0)
+    def left(self):
+        self.change(-10, 0)
+    def top(self):
+        self.change(0, 10)
+    def bottom(self):
+        self.change(0, -10)
 
-def whereIsCoordinate(coordinates):
-    if(not inside(coordinates)):
-        return "Wall"
-    elif (coordinates in snake):
-        return "Body"
-    elif (coordinates == food):  
-        return "Food"
-    else:
-        return "Empty"
+    def moveDirection(self, head, aim):
+        if(aim.x > 0):
+            return "Right"
+        elif(aim.x < 0):
+            return "Left"
+        elif(aim.y < 0):
+            return "Bottom"
+        else:
+            return "Top"
+
+    def whereIsCoordinate(self, coordinates):
+        if(not self.inside(coordinates)):
+            return "Wall"
+        elif (coordinates in self.snake):
+            return "Body"
+        elif (coordinates == self.food):  
+            return "Food"
+        else:
+            return "Empty"
